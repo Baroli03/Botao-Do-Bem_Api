ca18ec4315c39c06e03d3d129d3e85989a3d36a6
EDUARDO BAROLI SOUZA <eduardo.souza999@cs.up.edu.br>
2025-05-13 10:13:00 -0300

    Terminando o CRUD

--- Botaodobem/Program.cs
+++ Botaodobem/Program.cs
@@ -44,5 +44,49 @@ app.MapGet("/emocoes/{id}", async (int id, AppDbContext db) =>
     return emocoes is not null ? Results.Ok(emocoes) : Results.NotFound("Emocao não encontrado!");
 });
 
+app.MapGet("/emocoes/nome/{nome}", async (string nome, AppDbContext db) => 
+{
+    var emocoes = await db.Emocao.Where(e => e.Nome == nome).ToListAsync();
+    return emocoes.Any() ? Results.Ok(emocoes) : Results.NotFound();
+});
+
+app.MapPost("/emocoes", async (AppDbContext db, Emocao emocao) => 
+{
+    db.Emocao.Add(emocao);
+    await db.SaveChangesAsync();
+    return Results.Created($"emocoes/{emocao.Id}", emocao);
+});
+
+app.MapPut("/emocoes/{id}", async (int id, AppDbContext db, Emocao updatedEmocao) => 
+{
+    var EmocaoAtual = await db.Emocao.FindAsync(id);
+
+    if (EmocaoAtual == null)
+    {
+        return Results.NotFound("Emocao não encontrado!");
+    }
+
+    EmocaoAtual.Nome = updatedEmocao.Nome;
+    EmocaoAtual.Frase = updatedEmocao.Frase;
+
+    await db.SaveChangesAsync();
+
+    return Results.Ok(EmocaoAtual);
+});
+
+app.MapDelete("/emocoes/{id}", async (int id, AppDbContext db) => 
+{
+    var emocao = await db.Emocao.FindAsync(id);
+
+    if (emocao == null)
+    {
+        return Results.NotFound("Emoção não encontrado!");
+    }
+
+    db.Emocao.Remove(emocao);
+    await db.SaveChangesAsync();
+
+    return Results.Ok("Emoção Foi de base!");
+});
 
 app.Run();
