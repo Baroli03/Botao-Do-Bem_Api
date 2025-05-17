19d4d48718cd2da3e79e14ff288356d995231d88
Eduardo Baroli Souza <eduardo_Baroli@hotmail.com>
2025-05-17 12:35:32 -0300

    Criando estrutura do html

--- Botaodobem/script/script.js
+++ Botaodobem/script/script.js
@@ -1,11 +1,28 @@
-const btn = document.getElementById("Botao");
+const btn = document.getElementById("button-mode");
+const modeToggle = document.getElementById("mode-toggle")
+const h1 = document.getElementById("h1")
 let ligado = true
 
 btn.addEventListener("click", () =>{
     ligado = !ligado;
+
+    if (ligado) {
+        btn.classList.remove("creepster-regular")
+        btn.classList.add("cormorant-unicase-light")
+        h1.classList.remove("creepster-regular")
+        h1.classList.add("cormorant-unicase-light")
+    } else {
+        btn.classList.remove("cormorant-unicase-light")
+        btn.classList.add("creepster-regular")
+        h1.classList.remove("cormorant-unicase-light")
+        h1.classList.add("creepster-regular")
+    }
     btn.textContent = ligado ? "BEM" : "MAL";
+    btn.style.color = ligado ? "black" : "white";
     btn.style.backgroundColor = ligado ? "green" : "red";
     document.body.style.backgroundColor = ligado ?  "white" : "black";
-    btn.style.color = "white";
+    modeToggle.style.color = ligado ? "black" : "white";
+
+  
 }
 )
