3c62c7d58a84d8c486e5edcf64e9ebdbd339274a
Eduardo Baroli Souza <eduardo_Baroli@hotmail.com>
2025-05-17 15:00:49 -0300

    Adicionando um background foda

--- Botaodobem/script/script.js
+++ Botaodobem/script/script.js
@@ -11,17 +11,20 @@ btn.addEventListener("click", () =>{
         btn.classList.add("cormorant-unicase-light")
         h1.classList.remove("creepster-regular")
         h1.classList.add("cormorant-unicase-light")
+        document.body.classList.remove("maldito")
+        document.body.classList.add("divino")
     } else {
         btn.classList.remove("cormorant-unicase-light")
         btn.classList.add("creepster-regular")
         h1.classList.remove("cormorant-unicase-light")
         h1.classList.add("creepster-regular")
+        document.body.classList.remove("divino")
+        document.body.classList.add("maldito")
     }
     btn.textContent = ligado ? "BEM" : "MAL";
-    btn.style.color = ligado ? "black" : "white";
-    btn.style.backgroundColor = ligado ? "green" : "red";
+    btn.style.color = ligado ? "black" : "red";
     document.body.style.backgroundColor = ligado ?  "white" : "black";
-    modeToggle.style.color = ligado ? "black" : "white";
+    modeToggle.style.color = ligado ? "black" : "red";
 
   
 }
