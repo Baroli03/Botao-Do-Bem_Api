41c803ed01fea2feb8be25e72b819b6ab675ba09
Eduardo Baroli Souza <eduardo_Baroli@hotmail.com>
2025-05-19 14:19:57 -0300

    Dando forma ao frontend, adicionando botões e a parte escrita

--- Botaodobem/script/script.js
+++ Botaodobem/script/script.js
@@ -5,22 +5,17 @@ let ligado = true
 
 btn.addEventListener("click", () =>{
     ligado = !ligado;
+    // Divino
+    document.body.classList.toggle("divino")
+    btn.classList.toggle("cormorant-unicase-light")
+    h1.classList.toggle("cormorant-unicase-light")
 
-    if (ligado) {
-        btn.classList.remove("creepster-regular")
-        btn.classList.add("cormorant-unicase-light")
-        h1.classList.remove("creepster-regular")
-        h1.classList.add("cormorant-unicase-light")
-        document.body.classList.remove("maldito")
-        document.body.classList.add("divino")
-    } else {
-        btn.classList.remove("cormorant-unicase-light")
-        btn.classList.add("creepster-regular")
-        h1.classList.remove("cormorant-unicase-light")
-        h1.classList.add("creepster-regular")
-        document.body.classList.remove("divino")
-        document.body.classList.add("maldito")
-    }
+    // Maldito
+    document.body.classList.toggle("maldito")
+    h1.classList.toggle("creepster-regular")
+    btn.classList.toggle("creepster-regular")
+
+    
     btn.textContent = ligado ? "BEM" : "MAL";
     btn.style.color = ligado ? "black" : "red";
     document.body.style.backgroundColor = ligado ?  "white" : "black";
